d30059075919cd7eaf7260d0772b7412a556a1f8
JUAN E TENORIO ARZOLA <tenoriojuann@gmail.com>
2017-03-31 21:51:31 -0400

    Inserting into the database works
    dont ask me how...
    
    jk

--- public/js/converter.js
+++ public/js/converter.js
@@ -9,14 +9,19 @@ var config = {
   firebase.initializeApp(config);
       // Get a reference to the database service
   var database = firebase.database();
+  var storage = firebase.storage();
   
-  function newStore(isbn, book_name, author, term, course,professor, crn, required, 
+  
+  // Function to add new entries to the database
+  
+  function newStore(isbn, book_name, author, term, course,professor, crn, IDK, required, 
 			q_new, q_used, q_rental, q_ebook,pz_new,pz_used,pz_rental,pz_ebook,
 			summary) {
 				
 	var rootRef = database.ref();
 	var storeRef = rootRef.child("BOOKS");
 	
+	// DATABASE structure
 	var newStoreRef = storeRef.push();
 	newStoreRef.set({
     AUTHOR: author,
@@ -29,6 +34,9 @@ var config = {
 	TERM: term
   });
   
+	// This node has to be process separetly
+	// They will still appear in the database together with 
+	// The other book-data
 	var newPrizeRef = newStoreRef.child("PRIZE");
 	newPrizeRef.set({
 		EBOOK:pz_ebook,
@@ -38,6 +46,9 @@ var config = {
 	
   });
   
+  	// This node has to be process separetly
+	// They will still appear in the database together with 
+	// The other book-data
     var newQuantityRef = newStoreRef.child("QUANTITY");
 	newQuantityRef.set({
 		EBOOK:q_ebook,
@@ -46,4 +57,73 @@ var config = {
 		USED:q_used
 	
   });
+
 }
+
+  // Function to get the database in JSON format
+  // This is going to be used for the searching mechanism
+  function getJSON(){
+	  
+	  var rootRef = database.ref();
+	var storeRef = rootRef.child("BOOKS");
+	
+	  $.getJSON("https://bookstore-e0ecb.firebaseio.com/BOOKS.json", function(data){
+		 return data
+	  });
+  }
+  
+  // These next function handle the user uploading the books.csv file
+  function handleFiles(files) {
+      // Check for the various File API support.
+      if (window.FileReader) {
+          // FileReader are supported.
+          getAsText(files[0]);
+      } else {
+          alert('FileReader are not supported in this browser.');
+      }
+    }
+
+    function getAsText(fileToRead) {
+      var reader = new FileReader();
+      // Read file into memory as UTF-8      
+      reader.readAsText(fileToRead);
+      // Handle errors load
+      reader.onload = loadHandler;
+      reader.onerror = errorHandler;
+    }
+
+    function loadHandler(event) {
+      var csv = event.target.result;
+      processData(csv);
+    }
+
+	
+	// Once we got the file from the user
+	// We can process it and then add it to the database
+    function processData(csv) {
+		var result = "isbn, book_name, author, term, course, IDK, professor, crn, required, q_new, q_used, q_rental, q_ebook,pz_new,pz_used,pz_rental,pz_ebook,summary\n" + csv;  //now you have the header
+		var data = d3.csvParse(result);
+		//Now that we got the file we can send it to the database.
+		
+		var dataInObject = [];
+		for(var i =0; i < data.length; i ++){
+			for (var property in data[i]) {
+			
+				dataInObject.push(data[i][property]);
+			}
+			
+			newStore(dataInObject[0], dataInObject[1], dataInObject[2], dataInObject[3], dataInObject[4], dataInObject[5],
+					dataInObject[6], dataInObject[7], dataInObject[8], dataInObject[9], dataInObject[10], dataInObject[11],
+					dataInObject[12], dataInObject[13], dataInObject[14], dataInObject[15], dataInObject[16], dataInObject[17]);
+		}
+		
+		
+  
+    }
+
+	// ERRORS
+    function errorHandler(evt) {
+      if(evt.target.error.name == "NotReadableError") {
+          alert("Cannot read file !");
+      }
+    }
