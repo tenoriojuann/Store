369c806629599d3fa0aed3e95fe82ac166de9fb9
JUAN E TENORIO ARZOLA <tenoriojuann@gmail.com>
2017-04-17 19:57:45 -0400

    The summary is now searchable

--- public/js/search.js
+++ public/js/search.js
@@ -132,7 +132,7 @@ function receiveSearchRequest(){
       maxPatternLength: 32,
       minMatchCharLength: 1,
       // Keys to look for
-      keys: ['author','bookName','course', 'prof', 'crn']
+      keys: ['author','bookName','course', 'prof', 'crn', 'summary']
     };
 
     // Calling the Search object and passing the database and the search options
