4584fbc420a26a07d5e74e4e6df128827ffcd4fb
JUAN E TENORIO ARZOLA <tenoriojuann@gmail.com>
2017-04-21 23:52:32 -0400

    refactored newStore

--- public/js/converter.js
+++ public/js/converter.js
@@ -2,9 +2,7 @@
   
   // Function to add new entries to the database
   
-  function newStore(isbn, book_name, author, term, course, IDK, professor, crn, required, 
-			q_new, q_used, q_rental, q_ebook,pz_new,pz_used,pz_rental,pz_ebook,
-			summary) {
+  function newStore(data) {
 				
 	var rootRef = database.ref();
 	var storeRef = rootRef.child("books");
@@ -12,23 +10,23 @@
 	// DATABASE structure
 	var newStoreRef = storeRef.push();
 	newStoreRef.set({
-    author: author,
-    bookName: book_name,
-    course : course,
-    professor : professor,
-	crn : crn,
-	isbn : isbn,
-	required: required,
-	summary: summary,
-	term: term,
-	priceEbook:pz_ebook,
-	priceNew:pz_new,
-	priceRental:pz_rental,
-	priceUsed: pz_used,
-	quantityEbook:q_ebook,
-	quantityNew:q_new,
-	quantityRental:q_rental,
-	quantityUsed:q_used
+    author: data.author,
+    bookName: data.book_name,
+    course: data.course,
+    professor: data.professor,
+	crn: data.crn,
+	isbn: data.isbn,
+	required: data.required,
+	summary: data.summary,
+	term: data.term,
+	priceEbook: data.pc_ebook,
+	priceNew: data.pc_new,
+	priceRental: data.pc_rental,
+	priceUsed: data.pc_used,
+	quantityEbook: data.q_ebook,
+	quantityNew: data.q_new,
+	quantityRental: data.q_rental,
+	quantityUsed: data.q_used
   });
 
 }
@@ -74,27 +72,15 @@
     function processData(csv) {
     	// The file comes without headers by passing this string
     	//we will have the header added to the csv file
-		var result = "isbn,book_name,author,term,course,IDK,professor,crn,required,q_new,q_used,q_rental,q_ebook,pz_new,pz_used,pz_rental,pz_ebook,summary\n" + csv;  
+		var result = "isbn,book_name,author,term,course,IDK,professor,crn,required,q_new,q_used,q_rental,q_ebook,pc_new,pc_used,pc_rental,pc_ebook,summary\n" + csv;  
 		var data = d3.csvParse(result);
 		//Now that we got the file we can send it to the database.
-		
-		var dataInObject = [];
-		for(var i =0; i < data.length; i ++){
-			while(dataInObject.length > 0) {
-				dataInObject.pop();
-			}
-			for (var property in data[i]) {
-			
-				dataInObject.push(data[i][property]);
-			} 
 
-			// PERHAPS THIS WILL BE BETTER BY PASSING THE WHOLE OBJECT---I was a little tipy when this happened
-			newStore(dataInObject[0], dataInObject[1], dataInObject[2], dataInObject[3], dataInObject[4], dataInObject[5],
-					dataInObject[6], dataInObject[7], dataInObject[8], dataInObject[9], dataInObject[10], dataInObject[11],
-					dataInObject[12], dataInObject[13], dataInObject[14], dataInObject[15], dataInObject[16], dataInObject[17]); 
-		
+		for(var index = 0; index < data.length; index++){
+
+			newStore(data[index]);
 		}
-
+		
 		window.confirm("Database updated!");
     }
 
