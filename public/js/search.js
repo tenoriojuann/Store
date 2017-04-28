2386d8ba3b1444a3357b5b11e52e13db4a75fc07
dsundby10 <dsundby10@yahoo.com>
2017-04-27 21:24:50 -0400

    Search Results Display Price / Input / Qty with Colored txt for stock

--- public/js/search.js
+++ public/js/search.js
@@ -106,77 +106,111 @@ $('#search-table tr').each(function(index) {
               id: row.find('td:nth-last-child(2)').text()
           });
       };
+	  
       var rootRef = database.ref();
       // referencing the 'books' node
       var storeRef = rootRef.child("books");
       var id = row.find('td:nth-last-child(2)').text();
+	  
+	  var inStock = "In Stock"; //inStock var set to green
+		inStock = inStock.fontcolor("green");
+	  
+	  var outStock = "Out of Stock"; //outStock var set to red
+		outStock = outStock.fontcolor("red");
       
-      storeRef.child(id).once('value').then(function (book) {
-     // var strX = "Out of Stock";
-	 // var redX = "strX.fontcolor("red");
+	  storeRef.child(id).once('value').then(function (book) {
+		//Price and Quantity for New**************************************/
 		var mycell = row.find('td:nth-last-child(7)');
-        mycell[0].innerHTML = ("$" + book.val().priceNew);
+        mycell[0].innerHTML = ("$" + book.val().priceNew);//insert $ sign
         var input = document.createElement('input');
         input.type = "text";
+		//input.value = "0";  //Uncomment to make input values show as 0
         mycell.append(input);
         var quantity = document.createElement('p');
-        quantity.innerHTML = "<br>Quantity: " + book.val().quantityNew;
-        mycell.append(quantity);
-        console.log(mycell[0].innerHTML);
+		if(book.val().quantityNew <= 0 || book.val().quantityNew % 1 != 0)//check if qty less than or equal to 0, or if qty is an int
+		{
+			quantity = outStock;
+			book.val().quantityNew = quantity;
+			mycell.append("<br/>" + "<br>" + quantity);
+		}
+		else //print normal
+		{
+			quantity.innerHTML = "<br>" +inStock + ": " + book.val().quantityNew;
+			mycell.append(quantity);
+        }
+		console.log(mycell[0].innerHTML);
 		
+		//Price and Quantity for Used**************************************/
 		var mycell2= row.find('td:nth-last-child(6)');
-		mycell2[0].innerHTML = ("$" + book.val().priceUsed);
+		mycell2[0].innerHTML = ("$" + book.val().priceUsed);//insert $ sign
 		var input1 = document.createElement('input');
         input1.type = "text";
+		//input1.value = "0";  //Uncomment to make input values show as 0
         mycell2.append(input1);
         var quantity1 = document.createElement('p');
-        quantity1.innerHTML = "<br>Quantity: " + book.val().quantityUsed;
-        mycell2.append(quantity1);
+       
+		if(book.val().quantityUsed <= 0 || book.val().quantityUsed % 1 != 0)//check if qty less than or equal to 0, or if qty is an int
+		{
+			quantity1 = outStock;
+			book.val().quantityUsed = quantity1;
+			mycell2.append("<br/>" + "<br>" + quantity1);
+		}
+		else  //print normal
+		{
+			quantity1.innerHTML = "<br>" +inStock+ ": " + book.val().quantityUsed;
+			mycell2.append(quantity1);
+		}
+		console.log(mycell2[0].innerHTML);
 		
+		//Price and Quantity for Rental**************************************/
 		var mycell3= row.find('td:nth-last-child(5)');
-		mycell3[0].innerHTML = ("$" + book.val().priceRental);
+		mycell3[0].innerHTML = ("$" + book.val().priceRental); //insert $ sign
 		var input2 = document.createElement('input');
         input2.type = "text";
+		//input2.value = "0";  //Uncomment to make input values show as 0
         mycell3.append(input2);
         var quantity2 = document.createElement('p');
-		//if(book.val()quantityRental <= 0 || book.val()quantityRental % 1 !=0)
-		//{
-		//	quantity2 = redX;
-		//	book.val().quantityRental = quantity2;
-		//}
 		
-        quantity2.innerHTML = "<br>Quantity: " + book.val().quantityRental;
-        mycell3.append(quantity2);
+		if(book.val().quantityRental <= 0 || book.val().quantityRental % 1 != 0)//check if qty less than or equal to 0, or if qty is an int
+		{
+			quantity2=outStock;
+			book.val().quantityRental = quantity2;
+			mycell3.append("<br/>" + "<br>" + quantity2);
+		}
+		else  //print normal
+		{
+			quantity2.innerHTML = "<br>" +inStock+ ": " + book.val().quantityRental;
+			mycell3.append(quantity2);
+		}
+		console.log(mycell3[0].innerHTML);
 		
+		//price and quantity for eBook**************************************/
 		var mycell4= row.find('td:nth-last-child(4)');
-		mycell4[0].innerHTML = ("$" + book.val().priceEbook);
+		mycell4[0].innerHTML = ("$" + book.val().priceEbook);//insert $ sign
 		var input3 = document.createElement('input');
         input3.type = "text";
+		//input3.value = "0";  //Uncomment to make input values show as 0
         mycell4.append(input3);
         var quantity3 = document.createElement('p');
-		//if(book.val().quantityEbook > 0)
-		//{
-		//	quantity3 = "Unlimited";
-		//	book.val().quantityEbook = quantity3;
-		//}
-		//if(book.val().quantityEbook <= 0)
-		//{
-			//quantity3 = "redX";
-			//book.val().quantityEbook = quantity3;
-		//}
-        quantity3.innerHTML = "<br>Quantity: " + book.val().quantityEbook;
-        mycell4.append(quantity3);
-		
-		
-		
 
+		if(book.val().quantityEbook > 0) //Any value over 0 = unlimited stock
+		{
+			quantity3 = inStock + ": Unlimited";
+			book.val().quantityEbook = quantity3;
+		}
+		if(book.val().quantityEbook <= 0 || book.val().quantityEbook % 1 != 0) //Any int value under 0 = no stock
+		{
+			quantity3=outStock;
+			book.val().quantityEbook =quantity3;
+		}     
+        mycell4.append("<br/>" + "<br>" + quantity3);
+		console.log(mycell4[0].innerHTML);
       });
+	  
       var check = document.createElement('input');
       check.type = "checkbox";
-      row.find('td:first-child').replaceWith(img);
-      row.find('td:last-child').replaceWith(check);
-
-	  
+      row.find('td:first-child').replaceWith(img); //table first child set to image
+      row.find('td:last-child').replaceWith(check); //table last child set to check mark 
     });
   }
 });
